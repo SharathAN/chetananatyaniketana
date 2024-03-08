e808a454d623370d5c2d3e0fb0888bd8addd065e
SharathAN <63700833+SharathAN@users.noreply.github.com>
2024-03-09 00:09:35 +0530

    version2 major updates

---
+++ js/script.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+    loadHeader();
+    loadFooter();
+};
+// san
+function loadHeader() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', './Header.html', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById('headerFile').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+function loadFooter() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', './Footer.html', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById('footerFile').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+function highlightTabElement(elementId) {
+    waitForElement(5000, elementId, function (element) {
+        element.style.backgroundImage = "url('images/button-bgr-hover.jpg')";
+    });
+}
+
+function waitForElement(timeout, selector, callback) {
+    var elapsedTime = 0;
+    var interval = setInterval(function () {
+        var element = document.querySelector(selector);
+        elapsedTime += 100;
+        if (element) {
+            clearInterval(interval);
+            callback(element);
+        } else if (elapsedTime >= timeout) {
+            clearInterval(interval);
+            handleTimeout(selector, timeout);
+        }
+    }, 100); // Check every 100 milliseconds
+}
+
+function handleTimeout(selector, timeout) {
+    console.error(`Timeout occurred: Element ${selector} not found within ${timeout} milliseconds!`);
+}
+
+
+
+
+//other ref
+function sampleTestFn() {
+    fetch('./Footer.html').then(response => response.text()).then(html => {
+        document.getElementById('footerFile').innerHTML = html;
+    });
+    document.getElementById('coursesTab').style.backgroundImage= "url('images/button-bgr-hover.jpg')";
+}
+
+//san
